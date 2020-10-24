b8a9801aba1ef0b6f41454c77c59744d1b5f9fa9
KentoKashiwaya <kento@KentoKashiwayanoMacBook-Pro.local>
2020-10-25 02:34:17 +0900

    Responsive Tab Header

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -6,11 +6,58 @@ import logo from "../image/logo.png";
 function Header() {
   return (
     <div className="Header">
-      <Link to="/"><img src={logo} className="logo"/></Link>
-      <Link to="/about" className="menu about_menu">ABOUT</Link>
-      <Link to="/skil" className="menu skil_menu">SKIL</Link>
-      <Link to="/work" className="menu work_menu">WORK</Link>
-      <Link to="/contact" className="menu contact_menu">CONTACT</Link>
+      <Link to="/">
+        <img src={logo} className="logo" />
+      </Link>
+      <Link to="/about" className="menu about_menu pc_only">
+        ABOUT
+      </Link>
+      <Link to="/skil" className="menu skil_menu pc_only">
+        SKIL
+      </Link>
+      <Link to="/work" className="menu work_menu pc_only">
+        WORK
+      </Link>
+      <Link to="/contact" className="menu contact_menu pc_only">
+        CONTACT
+      </Link>
+
+      <nav className="leftNav sp_only">
+        <input id="menu" type="checkbox" />
+        <label for="menu" className="open">
+          ///
+        </label>
+        <label for="menu" className="back"></label>
+        <ul className="sideMenu">
+          <label for="menu">
+            <li>
+              <Link to="/" className="menu">
+                TOP
+              </Link>
+            </li>
+            <li>
+              <Link to="/about" className="menu about_menu">
+                ABOUT
+              </Link>
+            </li>
+            <li>
+              <Link to="/skil" className="menu skil_menu">
+                SKIL
+              </Link>
+            </li>
+            <li>
+              <Link to="/work" className="menu work_menu">
+                WORK
+              </Link>
+            </li>
+            <li>
+              <Link to="/contact" className="menu contact_menu">
+                CONTACT
+              </Link>
+            </li>
+          </label>
+        </ul>
+      </nav>
     </div>
   );
 }
