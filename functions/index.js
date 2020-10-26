cb686667e3e71f7f5118b0d23d66263a7de61298
KentoKashiwaya <kento@KentoKashiwayanoMacBook-Pro.local>
2020-10-26 22:27:57 +0900

    Setup Firebase

---
+++ functions/index.js
@@ -0,0 +1,5 @@
+const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+const nodemailer = require('nodemailer');
+const cors = require('cors')({origin: true});
+admin.initializeApp();
